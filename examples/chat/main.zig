c97cefba23fc0fe72f20085c6f60298cb34ff6d4
dravenk <longxianwen@outlook.com>
2025-01-26 19:13:51 +0800

    Update example

--- examples/chat/main.zig
+++ examples/chat/main.zig
@@ -1,20 +1,16 @@
 const std = @import("std");
 const Ollama = @import("ollama").Ollama;
-const RequestOptions = @import("ollama").RequestOptions;
-const OllamaResponse = @import("ollama").OllamaResponse;
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     var ollama = try Ollama.init(.{ .host = "localhost", .port = 11434, .allocator = allocator });
 
-    const message = [_]RequestOptions.chat.Message{
+    var req = try ollama.chat(.{ .model = "llama3.2", .messages = &.{
         .{ .role = "user", .content = "Why is the sky blue?" },
-    };
-
-    var req = try ollama.chat(.{ .model = "llama3.2", .messages = &message });
+    } });
     defer req.deinit();
 
-    const responses: []OllamaResponse = try ollama.full_response(&req);
+    const responses = try ollama.full_response(&req);
     for (responses) |response| {
         const json = try response.to_json(allocator);
         std.debug.print("{s}\n", .{json});
